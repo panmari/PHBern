3ff6df7ed2f0e8613bc2b5be0e781beac1c739a8
panmari <stefan.moser@students.unibe.ch>
2011-04-26 13:39:39 +0200

    Implemented Class PokerCombo
    Should make comparison between two combos easier!

--- CardGames/src/PokerApr16.java
+++ CardGames/src/PokerApr16.java
@@ -5,159 +5,174 @@
 /* Test:
  - Klicken auf obere Hand rechts (Kreuz 10)
  - Bewegt sich in mitte und Schaufel 6 kommet nach oben
-*/
-
+ */
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import ch.aplu.jcardgame.*;
 import ch.aplu.jcardgame.Hand.SortType;
 import ch.aplu.jgamegrid.Location;
 
-public class PokerApr16 extends CardGame
-{
-  public enum Suit
-  {
-    KREUZ, KARO, HERZ, PIK
-  }
+public class PokerApr16 extends CardGame {
+	public enum ComboType {
+		StraightFlush, Quads, Flush, FullHouse, Straight, Trips, Pair, SingleCard
+	}
+	public enum Suit {
+		KREUZ, KARO, HERZ, PIK
+	}
 
-  public enum Rank
-  {
-    ASS, KOENIG, DAME, BUBE, ZEHN, NEUN, ACHT, SIEBEN, SECHS
-  }
-  private Deck deck = new Deck(Suit.values(), Rank.values(), "cover");
-  private Hand[] hands;
-  private Hand burntCards;
-  private Hand stock;
-  private Location burntCardsLocation = new Location(100, 300);
-  private final int nbPlayers = 2;
-  private final int nbCards = 5;
-  private Location[] handLocations =
-  {
-    new Location(300, 100),
-    new Location(300, 500),
-  };
+	public enum Rank {
+		ASS, KOENIG, DAME, BUBE, ZEHN, NEUN, ACHT, SIEBEN, SECHS
+	}
 
-  public PokerApr16()
-  {
-    super(660, 660);
-    initBurntCards();
-    initHands();
-    stock = hands[2];
-    // Modified AP
-//    stock.setVerso(true);
-    stock.setView(this, new StackLayout(new Location(300, 300)));
-    stock.draw();
+	private Deck deck = new Deck(Suit.values(), Rank.values(), "cover");
+	private Hand[] hands;
+	private Hand burntCards;
+	private Hand stock;
+	private Location burntCardsLocation = new Location(100, 300);
+	private final int nbPlayers = 2;
+	private final int nbCards = 5;
+	private Location[] handLocations = { new Location(300, 100),
+			new Location(300, 500), };
 
-  }
+	public PokerApr16() {
+		super(660, 660);
+		initBurntCards();
+		initHands();
+		stock = hands[2];
+		stock.setVerso(true);
+		stock.setView(this, new StackLayout(new Location(300, 300)));
+		stock.draw();
+	}
 
-  private void initBurntCards()
-  {
-    burntCards = new Hand(deck);
-    burntCards.setView(this, new StackLayout(burntCardsLocation));
-  }
+	private void initBurntCards() {
+		burntCards = new Hand(deck);
+		burntCards.setView(this, new StackLayout(burntCardsLocation));
+	}
 
-  private void initHands()
-  {
-    // Modified AP
-    // To get reproducible conditions
-    hands = deck.dealingOut(nbPlayers, nbCards, false);
-//    hands = deck.dealingOut(nbPlayers, nbCards);
-    for (int i = 0; i < nbPlayers; i++)
-    {
-      final int k = i;
-      hands[i].addCardListener(new CardAdapter()
-      {
-        public void leftDoubleClicked(Card card)
-        {
-    // Modified AP
-//          card.setVerso(true);
-          card.transfer(burntCards, true);
-          Card newCard = stock.getLast();
-          newCard.transfer(hands[k], true);
-    // Modified AP
-//          newCard.setVerso(false);
-          System.out.println("Highest Value Hand " + k + "\n"
-            + getHighestCombo(hands[k]) + "\n");
-        }
-      });
-      hands[i].setTargetArea(new TargetArea(burntCardsLocation));
-      hands[i].setTouchEnabled(true);
-      hands[i].setView(this, new RowLayout(handLocations[i], 450));
-      hands[i].sort(SortType.RANKPRIORITY, false);
-      hands[i].draw();
-    }
-  }
+	private void initHands() {
+		hands = deck.dealingOut(nbPlayers, nbCards, false);
+		for (int i = 0; i < nbPlayers; i++) {
+			final int k = i;
+			hands[i].addCardListener(new CardAdapter() {
+				public void leftDoubleClicked(Card card) {
+					card.setVerso(true);
+					card.transfer(burntCards, true);
+					Card newCard = stock.getLast();
+					newCard.transfer(hands[k], true);
+					// Modified AP
+					newCard.setVerso(false);
+					System.out.println("Highest Value Hand " + k + "\n"
+							+ getHighestCombo(hands[k]) + "\n");
+				}
+			});
+			hands[i].setTargetArea(new TargetArea(burntCardsLocation));
+			hands[i].setTouchEnabled(true);
+			hands[i].setView(this, new RowLayout(handLocations[i], 450));
+			hands[i].sort(SortType.RANKPRIORITY, false);
+			hands[i].draw();
+		}
+	}
 
-  /**
-   * TODO: implement full houses and straights!
-   * test for bugs
-   *
-   * @param hand
-   * @return
-   */
-  private Hand getHighestCombo(Hand hand)
-  {
-    List<Hand> goodStuff = new ArrayList<Hand>();
-    for (Suit s : Suit.values())
-//      goodStuff.addAll(toArrayList(hand.getSequences(s, 5))); //straight flush
-// Modifed AP for tests
-      goodStuff.addAll(toArrayList(hand.getSequences(s, 4))); //straight flush
-    goodStuff.addAll(toArrayList(hand.getQuads()));
-    //insert full house here
-    //insert flush (alle 5 gleicher Suit)
-    goodStuff.addAll(toArrayList(hand.getSequences(5))); //straight
-    goodStuff.addAll(toArrayList(hand.getTrips()));
-    goodStuff.addAll(toArrayList(hand.getPairs()));
-    //somehow buggy:
+	/**
+	 * TODO: implement full houses and straights! test for bugs
+	 * 
+	 * @param hand
+	 * @return
+	 */
+	private PokerCombo getHighestCombo(Hand hand) {
+		for (Suit s : Suit.values())
+			if (hand.getSequences(s, 5).length > 0)
+				return new PokerCombo(hand.getSequences(s, 5)[0], 
+						ComboType.StraightFlush);
+		if(hand.getQuads().length > 0)
+			return new PokerCombo(hand.getQuads()[0], 
+					ComboType.Quads);
+		
+		if(!getFlush(hand).isEmpty())
+			return new PokerCombo(getFlush(hand).get(0), 
+				ComboType.Flush);
+		
+		if (getHighestFullHouse(hand) != null)
+			return new PokerCombo(getHighestFullHouse(hand), 
+					ComboType.FullHouse);
+		
+		if (hand.getSequences(5).length > 0) // straight
+			return new PokerCombo(hand.getSequences(5)[0], 
+					ComboType.Straight);
+		
+		if (hand.getTrips().length > 0)
+			return new PokerCombo(hand.getTrips()[0], 
+					ComboType.Trips);
+		
+		if (hand.getPairs().length > 0)
+			return new PokerCombo(hand.getPairs()[0], 
+					ComboType.Pair);
 
-    //This line makes problems:
-    Hand handHiCard = new Hand(deck);
-    int posHighestCard = hand.getMaxPosition(SortType.RANKPRIORITY);
-    Card hiCard = hand.get(posHighestCard).clone();
-    // AP: hiCard immer noch in hand, da gecloned, siehe:
-    System.out.println("hand: " + hand);
+		Hand handHiCard = new Hand(deck);
+		int posHighestCard = hand.getMaxPosition(SortType.RANKPRIORITY);
+		Card hiCard = hand.get(posHighestCard).clone();
+		handHiCard.insert(hiCard, false);
+		return new PokerCombo(handHiCard, ComboType.SingleCard);
+	}
 
-    // AP: hiCard ist dupliziert, aber sie bleibt in hand drin, je nachdem
-    // was du damit machst, kann es Schwierigkeiten geben
-    handHiCard.insert(hiCard, false);
-    System.out.println("handHiCard: " + handHiCard);
-    goodStuff.add(handHiCard);
+	private List<Hand> getFlush(Hand hand) {
+		LinkedList<Hand> flushes = new LinkedList<Hand>();
+		for (Suit t: Suit.values()) {
+			int cardCounter = 0;
+			Hand currentCheckingCards = new Hand(deck);
+			for (Card a: hand.getCardList())  {
+				if (a.getSuit() == t) {
+					currentCheckingCards.insert(a.clone(), false);
+					cardCounter++;
+					if (cardCounter == 5) {
+						flushes.add(currentCheckingCards);
+						break;
+					}
+				}
+				
+			}
+		}
+		return flushes;
+	}
 
-    // For tests:
-    System.out.println("All hands in goodStuff:");
-    for (Hand h: goodStuff)
-      System.out.println(h);
-  
-    // Modified AP
-//    return goodStuff.get(0);
-    return goodStuff.size() == 0 ? null : goodStuff.get(0);
-  }
+	private int compareCombos(PokerCombo combo1, PokerCombo combo2) {
+		return 0;
+	}
 
-  /**
-   * TODO: generate an int value to a given Combo
-   * @param combo
-   * @return
-   */
-  private int getValueOfCombo(Hand combo)
-  {
-    return 0;
-  }
+	public static void main(String[] args) {
+		new PokerApr16();
+	}
 
-  public static void main(String[] args)
-  {
-    new PokerApr16();
-  }
-
-  private List<Hand> toArrayList(Hand[] hands)
-  {
-    return Arrays.asList(hands);
-  }
-
-  private List<Hand> getFullHouse()
-  {
-    return null;
-  }
+	private Hand getHighestFullHouse(Hand hand) {
+		Hand fullHouse = null;
+		Hand[] trips = hand.getTrips();
+		Hand[] pairs = hand.getPairs();
+		if (trips.length > 0 && pairs.length > 0) {
+			fullHouse = new Hand(this.deck);
+			fullHouse.insert(trips[0], false);
+			fullHouse.insert(pairs[0], false);
+		}
+		return fullHouse;
+	}
+	
+	class PokerCombo {
+		Hand hand;
+		ComboType comboType;
+		
+		public PokerCombo(Hand hand, ComboType comboType) {
+			this.hand = hand;
+			this.comboType = comboType;
+		}
+		
+		public String toString() {
+			String str = "Type of Combo: " + comboType + "\n";
+			str += hand;
+			return str;
+		}
+	}
 }
