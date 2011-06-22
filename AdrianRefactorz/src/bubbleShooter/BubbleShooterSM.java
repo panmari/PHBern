c77d2f8a13bb55e3e5790c98fd458168156e93a2
panmari <stefan.moser@students.unibe.ch>
2011-06-22 12:20:32 +0200

    BubbleShooter: tried some new way of validating the Position
    going back to previous version for now..

--- AdrianRefactorz/src/bubbleShooter/BubbleShooterSM.java
+++ AdrianRefactorz/src/bubbleShooter/BubbleShooterSM.java
@@ -25,7 +25,7 @@ public class BubbleShooterSM extends GameGrid implements GGMouseListener {
 			bubblePreviewLocations.add(new Location(x, 36));
 		for (Location loc : bubblePreviewLocations)
 			addRandomBubble(loc);
-		// initialize Bubble field:
+		// initialize Bubblefield:
 		for (int y = 1; y < 6; y++) {
 			for (int x = y; x < 37 - y; x += 2)
 				addRandomBubble(new Location(x, 2 * y - 1));
@@ -118,12 +118,12 @@ class Bubble extends Actor {
 	}
 
 	/**
-	 * Handles the arriving of the bubble. at the Field of Bubbles
+	 * Handles the arriving of the bubble at the bubblefield.
 	 */
 	@Override
-	public int collide(Actor a1, Actor a2) {
+	public int collide(Actor shootActor, Actor hitActor) {
 		setActEnabled(false);
-		setLocation(validateLocation(getLocation(), x));
+		setLocation(validateLocation(shootActor, hitActor));
 		addActorCollisionListener(null);
 		this.setLocationOffset(new Point(0, 0)); // centers Bubble in Grid
 		removeSameColorNeighbours(this);
@@ -143,6 +143,24 @@ class Bubble extends Actor {
 		y = vy * acceleration + y;
 		setPixelLocation(new Point((int) x, (int) y));
 	}
+	
+	private Location validateLocation(Actor shootActor, Actor hitActor) {
+		Point shootPoint = shootActor.getPixelLocation();
+		Point hitPoint = hitActor.getPixelLocation();
+		Location hitLoc = hitActor.getLocation();
+		int transX = calculateTranslation(shootPoint.x - hitPoint.x);
+		int transY = calculateTranslation(shootPoint.x - hitPoint.y);
+		return new Location(hitLoc.x + transX, hitLoc.y + transY);
+	}
+	
+	private int calculateTranslation(int difference) {
+		if (difference > 10) //east
+			return 2;
+		else if (difference < 10 && difference > -10)
+			return 0;
+		else return -2;
+	}
+	
 
 	/**
 	 * Transforms the given Location into a valid location of the 
@@ -150,9 +168,9 @@ class Bubble extends Actor {
 	 * every second row starting with 1 changes behaviour on the column-grid
 	 * To see more: Change the GameGrid Constructor so it shows the grid.
 	 */
-	private Location validateLocation(Location loc, double x) {
+	private Location setOnValidLocation(Location loc) {
 		if (loc.y % 2 == 0)
-			return validateLocation(new Location(loc.x, loc.y + 1), x);
+			return setOnValidLocation(new Location(loc.x, loc.y + 1));
 		else if ((loc.y % 4 == 1 && loc.x % 2 == 1)
 				|| (loc.y % 4 == 3 && loc.x % 2 == 0))
 			return loc;
@@ -204,12 +222,12 @@ class Pointer extends Actor {
 	public Pointer() {
 		super(true, "sprites/pointer.png");
 		// this.setLocationOffset(new Point(0,-80));
-		// edited picture so it fits
+		// edited picture so it fits, so the line above
+		// isn't necessary anymore.
 	}
 
 	public void act() {
 		Location mousePos = gameGrid.getMouseLocation();
-		// SM: could be done preciser
 		if (mousePos != null) { // is null if mouse is outside of grid
 			double dirToMouse = getLocation().getDirectionTo(mousePos);
 			setDirection(dirToMouse);
