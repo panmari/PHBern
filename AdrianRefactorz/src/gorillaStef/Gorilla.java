d1a79be0d34314dea3cece8c2302f59424489c03
panmari <stefan.moser@students.unibe.ch>
2011-07-14 14:15:38 +0200

    Gorilla-Game is now actually playable
    Fixed lots of stuff, Gorilla on the right side behaves now correctly...

--- AdrianRefactorz/src/gorillaStef/Gorilla.java
+++ AdrianRefactorz/src/gorillaStef/Gorilla.java
@@ -1,10 +1,60 @@
 package gorillaStef;
 
+import javax.swing.JOptionPane;
+
 import ch.aplu.jgamegrid.Actor;
+import ch.aplu.jgamegrid.Location;
 
 public class Gorilla extends Actor{
 
-	public Gorilla() {
+	private Location lunchLoc;
+	private GorillaMain gg;
+	
+	public Gorilla(GorillaMain gg, Location bananaLunchLoc) {
 		 super("sprites/gorilla.png", 2);
+		 this.lunchLoc = bananaLunchLoc;
+		 this.gg = gg;
+	}
+	
+	public void reset() {
+		 if (isRightGorilla())
+			 setHorzMirror(true);
+	}
+	
+	public void lunchBanana() {
+		show(1);
+		double angle = Math.toRadians(requestNumber("Angle: "));
+		double speed = requestNumber("Speed: ");
+		double vx = Math.cos(angle)*speed;
+		double vy = Math.sin(angle)*speed;
+		if (isRightGorilla()) //we're left gorilla
+			vx = -vx;
+		gg.addActor(new Banana(vx, -vy), lunchLoc);
+		show(0);
+	}
+	
+	private boolean isRightGorilla() {
+		return getX() > gg.nbHorzCells/2;
+	}
+	
+	private double requestNumber(String prompt) {
+		double number = 0;
+		Boolean invalidEntry = true;
+
+		while (invalidEntry) {
+			try {
+				String entry = JOptionPane.showInputDialog(null, prompt, "",
+						JOptionPane.PLAIN_MESSAGE);
+				number = Double.parseDouble(entry);
+				invalidEntry = false;
+			} catch (NumberFormatException e) {
+				System.out.println("Entry was not valid!");
+			} catch (NullPointerException e) {
+				gg.setTitle("Quiting Game");
+				delay(2000);
+				System.exit(0);
+			}
+		}
+		return number;
 	}
 }
--- AdrianRefactorz/src/gorillaStef/GorillaMain.java
+++ AdrianRefactorz/src/gorillaStef/GorillaMain.java
@@ -1,7 +1,5 @@
 package gorillaStef;
 
-import javax.swing.JOptionPane;
-
 import ch.aplu.jgamegrid.GameGrid;
 import ch.aplu.jgamegrid.Location;
 
@@ -10,39 +8,28 @@ public class GorillaMain extends GameGrid {
 	WindSock ws = new WindSock();
 	Location[] gorillaLoc = {new Location(35,565), new Location(1165,565)};
 	Location[] bananaLunchLoc = {new Location(70, 535), new Location(1130, 535)};
+	Gorilla[] players = new Gorilla[2];
+	private int currentPlayer = 0;
 	
 	public GorillaMain() {
 		super(1200, 600, 1, null, "sprites/townBig.jpg", true);
 		setSimulationPeriod(30);
 		addActor(ws, new Location(nbHorzCells/2, 40));
-		ws.setRandomWind();
-		addActor(new Banana(50, -100), bananaLunchLoc[0]);
-		for (Location loc: gorillaLoc)
-			addActor(new Gorilla(), loc);
+		for (int i = 0; i < 2; i++) {
+			players[i] = new Gorilla(this, bananaLunchLoc[i]);
+			addActor(players[i], gorillaLoc[i]);
+		}
 		show();
-		//doRun();
+		doRun();
+		while(true) {
+			ws.setRandomWind();
+			players[currentPlayer].lunchBanana();
+			currentPlayer = (currentPlayer + 1) % 2;
+		}
 	}
 	
 	public void act() {
-		if (getNbCycles() % 100 == 0)
-			ws.setRandomWind();
-	}
-
-	private double requestNumber(String prompt) {
-		double number = 0;
-		Boolean invalidEntry = true;
-
-		while (invalidEntry) {
-			try {
-				String entry = JOptionPane.showInputDialog(null, prompt, "",
-						JOptionPane.PLAIN_MESSAGE);
-				number = Double.parseDouble(entry);
-				invalidEntry = false;
-			} catch (NumberFormatException e) {
-				System.out.println("Entry was not valid!");
-			}
-		}
-		return number;
+			
 	}
 	
 	public static void main(String[] args) {
