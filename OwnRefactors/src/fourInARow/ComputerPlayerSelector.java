c95a8d1355f2659dd4e2aec1c61d3b7b499ef18d
panmari <stefan.moser@students.unibe.ch>
2011-09-18 11:15:48 +0200

    Completed changes on ComputerPlayerSelector of FourInARow

--- OwnRefactors/src/fourInARow/ComputerPlayerSelector.java
+++ OwnRefactors/src/fourInARow/ComputerPlayerSelector.java
@@ -1,3 +1,4 @@
+
 package fourInARow;
 
 import java.awt.Color;
@@ -6,69 +7,77 @@ import java.util.List;
 
 import ch.aplu.jgamegrid.*;
 
-public class ComputerPlayerSelector implements GGButtonListener{
+public class ComputerPlayerSelector implements GGButtonListener
+{
+  private GameGrid dlg;
+  private List<ComputerPlayer> availableCP = new LinkedList<ComputerPlayer>();
+  private GGRadioButtonGroup rbGroup;
+  private FourInARowVsComputer gg;
 
-	private GameGrid dlg;
-	private List<ComputerPlayer> availableCP = new LinkedList<ComputerPlayer>();
-	private GGRadioButtonGroup rbGroup;
-	private FourInARowVsComputer gg;
-	
-	public ComputerPlayerSelector(FourInARowVsComputer gg, 
-			ArrayManager am, int player) {
-		this.gg = gg;
-		createComputerPlayers(am, player);
-		createRadioButtons();
-	}
-	
-	  private void showDialog()
-	  {
-	    dlg = new GameGrid(15, availableCP.size() + 2, 30, null, false);
-	    dlg.setTitle("Select computer player");
-	    dlg.setBgColor(Color.white);
-	    dlg.show();
-	  }
+  public ComputerPlayerSelector(FourInARowVsComputer gg,
+    ArrayManager am, int player)
+  {
+    this.gg = gg;
+    createComputerPlayers(am, player);
+    showDialog();
+    createRadioButtons();
+  }
 
-	private void createComputerPlayers(ArrayManager am, int player) {
-		availableCP.add(new EasyBot(am, player));
-		availableCP.add(new DBot(am, player));
-		availableCP.add(new MMBot(am, player));
-	}
-	
-	private void createRadioButtons() {
-		rbGroup = new GGRadioButtonGroup();
-		int y = 0;
-		for (ComputerPlayer cp: availableCP) {
-			GGRadioButton rb = new GGRadioButton(cp.getNameAndDescription());
-			dlg.addActor(rb, new Location(0, y));
-			rbGroup.add(rb);
-			y++;
-		}
-		GGButton okBtn = new GGButton("sprites/ok.gif", true);
-		okBtn.addButtonListener(this);
-		dlg.addActor(okBtn, new Location(dlg.nbHorzCells - 2, y));
-	}
-	@Override
-	public void buttonClicked(GGButton button) {
-		int counter = 0;
-		//this is ugly:
-		for (GGRadioButton rb: rbGroup.getButtons()) {
-			if (rb == rbGroup.getSelectedButton()) {
-				gg.setComputerPlayer(availableCP.get(counter));
-				gg.show();
-				dlg.hide();
-			}
-			counter++;
-		}
-	}
-	@Override
-	public void buttonPressed(GGButton button) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void buttonReleased(GGButton button) {
-		// TODO Auto-generated method stub
-		
-	}
+  private void showDialog()
+  {
+    dlg = new GameGrid(15, availableCP.size() + 2, 30, null, false);
+    dlg.setTitle("Select computer player");
+    dlg.setBgColor(Color.white);
+    dlg.show();
+  }
 
+  private void createComputerPlayers(ArrayManager am, int player)
+  {
+    availableCP.add(new EasyBot(am, player));
+    availableCP.add(new DBot(am, player));
+    availableCP.add(new MMBot(am, player));
+  }
+
+  private void createRadioButtons()
+  {
+    rbGroup = new GGRadioButtonGroup();
+    int y = 0;
+    for (ComputerPlayer cp : availableCP)
+    {
+      GGRadioButton rb = new GGRadioButton(cp.getNameAndDescription());
+      dlg.addActor(rb, new Location(0, y));
+      rbGroup.add(rb);
+      y++;
+    }
+    GGButton okBtn = new GGButton("sprites/ok.gif", true);
+    okBtn.addButtonListener(this);
+    dlg.addActor(okBtn, new Location(dlg.getNbHorzCells() - 2, y));
+  }
+
+  public void buttonClicked(GGButton button)
+  {
+    int n = getSelectedButtonId();
+    if (n != -1)
+    {
+      gg.setComputerPlayer(availableCP.get(n));
+      gg.show();
+      dlg.hide();
+    }
+  }
+
+  private int getSelectedButtonId()
+  {
+    for (int i = 0; i < availableCP.size(); i++)
+      if (rbGroup.getSelectedButton() == rbGroup.getButtons().get(i))
+        return i;
+    return -1;
+  }
+
+  public void buttonPressed(GGButton button)
+  {
+  }
+
+  public void buttonReleased(GGButton button)
+  {
+  }
 }
