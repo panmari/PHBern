19db57547867c3b5adc1c12d61834ca6b4f28e52
panmari <smmuzi@gmail.com>
2012-10-09 16:35:30 +0200

    deleted many unnecessary files

--- trickyTurtles/src/sprites/resize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-counter=1
-root=karteklein
-resolution=164x164
-for i in `ls -1 ./*.gif`; do
-	echo "Now working on $i"
-	convert -resize $resolution $i ${root}_${counter}.gif
-	counter=`expr $counter + 1`
-done
