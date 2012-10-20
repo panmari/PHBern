d33d193c8577c015db4e889c2a1f33b27a3325a5
panmari <smmuzi@gmail.com>
2012-10-20 13:30:38 +0200

    getClosestObstacle implemented, kinda works

--- Triangles/src/models/ViewingCone.java
+++ Triangles/src/models/ViewingCone.java
@@ -1,18 +1,35 @@
 package models;
 
+import java.util.LinkedList;
+
 import ch.aplu.jgamegrid.GGVector;
 
 public class ViewingCone extends Triangle{
 
 	private GGVector standPoint;
-
+	private LinkedList<Triangle> obstacles;
+	
 	public ViewingCone(GGVector a, GGVector b, GGVector c) {
 		super(a, b, c);
 		this.standPoint = a;
+		this.obstacles = new LinkedList<Triangle>();
 	}
-
-	public GGVector closestPointTo(Triangle t) {
-		
-		return new GGVector();
+	
+	public void addObstacle(Triangle t) {
+		obstacles.add(t);
+	}
+	
+	public GGVector getClosestObstacle() {
+		//TODO add special case, where intersection of viewing cone & obstacle is closest value
+		GGVector best = new GGVector(Double.MAX_VALUE, Double.MAX_VALUE);
+		for (Triangle t: obstacles) {
+			GGVector candidate = t.closestPointTo(standPoint);
+			//TODO: add isShorterThan for Vectors
+			if (liesInside(candidate) && 
+					candidate.sub(standPoint).magnitude2() < best.sub(candidate).magnitude2())
+				best = candidate;
+		}
+			
+		return best;
 	}
 }
