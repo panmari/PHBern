9908b1b17ec3cb24ae1fe17b3a8ec53c79972637
panmari <smmuzi@gmail.com>
2012-12-28 17:45:07 +0100

    fix: only to one side infinite!

--- Triangles/src/models/LineSegment.java
+++ Triangles/src/models/LineSegment.java
@@ -20,7 +20,7 @@ public class LineSegment {
 	}
 	
 	public GGVector getPointOnLineSegment(double scaleDir) {
-		if ((scaleDir < 0 || scaleDir > 1) && !infinite)
+		if (scaleDir < 0 || (scaleDir > 1 && !infinite))
 			throw new IllegalArgumentException("Point would not be on line");
 		return start.add(direction.mult(scaleDir));
 	}
@@ -30,14 +30,14 @@ public class LineSegment {
 			return null; // lines are parallel
 		double magL = cross(other.start.sub(this.start), other.direction)/cross(this.direction, other.direction);
 		double magTl = cross(other.start.sub(this.start), this.direction)/cross(this.direction, other.direction);
-		if ((this.infinite || isBetweenZeroAndOne(magL)) && (isBetweenZeroAndOne(magTl) || other.infinite)) {
+		if (isOnLineSegment(magL, this.infinite) && isOnLineSegment(magTl, other.infinite)) {
 			return this.getPointOnLineSegment(magL);
 		} else
 			return null; // line segments do not intersect
 	}
 
-	private boolean isBetweenZeroAndOne(double d) {
-		return d <= 1 && d >= 0;
+	private boolean isOnLineSegment(double d, boolean infinite) {
+		return (infinite || d <= 1) && d >= 0;
 	}
 
 	private double cross(GGVector a, GGVector b) {
